15eed8a6ea5011d2b0f97ed8ed2438f8e6950358
123AbhiRaghav <abhiraghavd@gmail.com>
2024-05-11 21:11:32 +0530

    update

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const parser = require("body-parser");
 const app = express();
 app.use(parser.json());
 
-const PORT = process.env.PORT || 5000;
-const HOSTNAME = "0.0.0.0";
 
 require("dotenv").config()
-console.log(process.env)
+const PORT = process.env.PORT
+const HOSTNAME = process.env.HOSTNAME
 
 app.listen(PORT, HOSTNAME, () => {
-  console.log("Server started");
+  console.log(`Server started at http://${HOSTNAME}:${PORT}`);
 });
 
 app.post("/booking", (req, res) => {
